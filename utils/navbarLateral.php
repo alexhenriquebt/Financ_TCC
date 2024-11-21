39ebf8be3e0b06cab0b2e2c47f73f54bbae060f8
alexHenr0311 <alexagua42@gmail.com>
2024-11-21 09:52:10 -0300

    Home nova

--- utils/navbarLateral.php
+++ utils/navbarLateral.php
@@ -16,63 +16,66 @@ $base_url = rtrim($base_url, '/view');
 <div class="navbar-lateral text-center m-0">
   <div class="row g-4">
     <div class="col-12">
-      <a
-        href="<?php echo $base_url; ?>/home.php"
-        data-bs-toggle="popover"
-        data-bs-trigger="hover focus"
+      <a href="<?php echo $base_url; ?>/home.php" data-bs-toggle="popover" data-bs-trigger="hover focus"
         data-bs-content="Voltar para a tela inicial">
-        <img src="../assets/images/iconTituloPg.png" alt="" class="img-fluid" />
-        <p>Home</p>
+        <div>
+          <img src="../assets/images/iconTituloPg.png" alt="" class="img-fluid" />
+          <p>Home</p>
+        </div>
       </a>
     </div>
     <div class="col-12">
-      <a
-        href="<?php echo $base_url; ?>/centroCusto.php"
-        data-bs-toggle="popover"
-        data-bs-trigger="hover focus"
+      <a href="<?php echo $base_url; ?>/centroCusto.php" data-bs-toggle="popover" data-bs-trigger="hover focus"
         data-bs-content="Visualize e gerencie suas despesas e receitas">
-        <i class="bi bi-receipt"></i>
-        <p>Contas</p>
+        <div>
+          <i class="bi bi-receipt"></i>
+          <p>Contas</p>
+        </div>
       </a>
     </div>
     <div class="col-12">
-      <a
-        href="<?php echo $base_url; ?>/metas.php"
-        data-bs-toggle="popover"
-        data-bs-trigger="hover focus"
+      <a href="<?php echo $base_url; ?>/metas.php" data-bs-toggle="popover" data-bs-trigger="hover focus"
         data-bs-content="Defina metas">
-        <i class="bi bi-flag"></i>
-        <p>Metas</p>
+        <div>
+          <i class="bi bi-flag"></i>
+          <p>Metas</p>
+        </div>
       </a>
     </div>
     <div class="col-12">
-      <a
-        href="<?php echo $base_url; ?>/relatorios.php"
-        data-bs-toggle="popover"
-        data-bs-trigger="hover focus"
+      <a href="<?php echo $base_url; ?>/relatorios.php" data-bs-toggle="popover" data-bs-trigger="hover focus"
         data-bs-content="Veja um resumo de suas finanças">
-        <i class="bi bi-graph-up"></i>
-        <p>Relatórios</p>
+        <div>
+          <i class="bi bi-graph-up"></i>
+          <p>Relatórios</p>
+        </div>
       </a>
     </div>
     <div class="col-12">
-      <a
-        href="<?php echo $base_url; ?>/configuracoes.php"
-        data-bs-toggle="popover"
-        data-bs-trigger="hover focus"
+      <a href="<?php echo $base_url; ?>/configuracoes.php" data-bs-toggle="popover" data-bs-trigger="hover focus"
         data-bs-content="Configurações">
-        <i class="bi bi-gear"></i>
-        <p>Configurações</p>
+        <div>
+          <i class="bi bi-gear"></i>
+          <p>Configurações</p>
+        </div>
       </a>
     </div>
     <div class="col-12">
-      <a
-        href="<?php echo $base_url; ?>/ajuda.php"
-        data-bs-toggle="popover"
-        data-bs-trigger="hover focus"
+      <a href="<?php echo $base_url; ?>/ajuda.php" data-bs-toggle="popover" data-bs-trigger="hover focus"
         data-bs-content="Encontre como resolver seus problemas e dúvidas">
-        <i class="bi bi-info-circle"></i>
-        <p>Ajuda</p>
+        <div>
+          <i class="bi bi-info-circle"></i>
+          <p>Ajuda</p>
+        </div>
+      </a>
+    </div>
+    <div class="col-12">
+      <a href="<?php echo $base_url; ?>/blog.php" data-bs-toggle="popover" data-bs-trigger="hover focus"
+        data-bs-content="Veja dicas para melhorar suas finanças">
+        <div>
+          <i class="bi bi-card-text"></i>
+          <p>Blog</p>
+        </div>
       </a>
     </div>
   </div>
--- views/home.php
+++ views/home.php
@@ -1,33 +1,10 @@
- <?php
-// require_once '../model/validarAcesso.php';
-//require_once '../model/classeCentroCusto.php';
-//require_once '../model/classeCategoria.php';
-//$classeCentroCusto = new CentroCusto();
-//$classeCategoria = new Categoria();
-//$princCentroCusto = $classeCentroCusto->princCentroCusto();
-
-//Se existe despesa
-//if (count($princCentroCusto) > 0) {
-//  $mensagemDespesa = 'true';
-//} else {
-//  $mensagemDespesa = 'false';
-//}
-
-// valida o acesso do usuário!
-//require_once '../model/validador_acesso.php';
+<?php
+ require_once '../model/validarAcesso.php';
+ require_once '../model/classeCentroCusto.php';
+ $classeCentroCusto = new CentroCusto();
 //pega o nome do usuário
-//$usuario = $_SESSION['usuario'];
-
-// Calcula o total das despesas
-//$totalDespesas = array_sum(array_column($classeCentroCusto->buscarDespesas(), 'cenValor'));
-
-// Calcula o total das receitas
-//$totalReceitas = array_sum(array_column($classeCentroCusto->buscarReceitas(), 'cenValor'));
-
-//$historicoPatrimonio = [];
-//$patrimonio = $totalReceitas - $totalDespesas;
-//$historicoPatrimonio[] = $patrimonio;
-//?> 
+ $usuario = $_SESSION['usuario'];
+?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -35,22 +12,16 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link
-    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
-    rel="stylesheet"
-    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
-    crossorigin="anonymous" />
-  <link
-    rel="stylesheet"
-    href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
   <link rel="stylesheet" href="../css/home.css" />
   <link rel="shortcut icon" href="../assets/images/iconTituloPg.png" />
   <title>Home</title>
-  <script
-    src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
     crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
 
 <body>
@@ -68,19 +39,16 @@
           <button class="btn btn-danger" type="submit">Pesquisar</button>
         </form>
       </div>
-      <div class="container">
+      <div class="container mb-3">
         <div class="row bloco-saudacoes text-center">
           <div class="col-6">
             <p>
               Oi,
-              <!-- <?php echo $usuario ?> -->
+               <?php echo $usuario ?>
             </p>
           </div>
           <div class="col-6">
-            <img
-              src="../assets/images/pessoaAcenandoSemFundo.png"
-              alt="pessoa acenando"
-              class="img-fluid" />
+            <img src="../assets/images/pessoaAcenandoSemFundo.png" alt="pessoa acenando" class="img-fluid" />
           </div>
         </div>
 
@@ -88,187 +56,132 @@
         <div class="bloco-saudacoes-mobile">
           <p class="display-6">
             Oi,
-            <!-- <?php
+            <?php
             echo $usuario
-            ?> -->
+            ?>
           </p>
         </div>
         <!-- -------- Bloco de saudações para mobile --------- -->
       </div>
 
-      <div class="container p-5">
-        <div class="row">
-          <!-- <div class="col-12 col-md-6">
-             gráfico de linha início
-            <div class="grafico-barra">
-              <canvas id="bar_chart"></canvas>
+      <div class="container mt-3 mb-5">
+        <div class="row g-3">
+
+          <div class="col-12 col-lg-6">
+            <div class="container-estilizado">
+              <h5>Despesas do mês</h5>
+              <div class="container mt-5 mb-0">
+                <div class="row">
+                  <div class="col-12 col-md-6 p-3">
+                    <canvas id="despesas"></canvas>
+                  </div>
+                  <div class="col-12 col-md-6">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quae quasi minima autem est ipsa repellat veritatis repellendus debitis accusantium.</p>
+                  </div>
+                </div>
+              </div>
             </div>
-            gráfico de linha fim
-             tela MOBILE
-            <div class="dados-pratimonio">
-           <h5><?php echo 'Patrimônio atual: R$' . number_format($patrimonio, 2, ",", "."); ?></h5>
-            </div>
-             tela MOBILE
-          </div> -->
 
-          <div class="col-12 col-md-6">
-            <div class="container h-100">
-              <div class="row h-100">
 
-                <!-- <?php if ($mensagemDespesa == 'true') {
-                  foreach ($princDespesas as $index => $resultado) {
-                ?> -->
-
-                    <div class="col-12 col-md-6 col-lg-4">
-                      <a href="">
-                        <div class="card text-center card-despesa">
-                          <div class="card-body">
-                            <!-- <h5 class="card-title"><?php echo $resultado['desNome'] ?></h5> -->
-                            <!-- <p class="card-text"><?php echo $resultado['desValor'] ?></p> -->
-                          </div>
-                        </div>
-                      </a>
-                    </div>
-
-                  <!-- <?php
-                  } //terrmina o foreach
-                } //termina o if($mensagem)
-                else {
-                  ?> 
-                <?php
-                }
-                ?> -->
+            <div class="container-estilizado mt-3">
+              <h5>Receitas do mês</h5>
+              <div class="container mt-5 mb-0">
+                <div class="row">
+                  <div class="col-12 col-lg-6 p-3">
+                    <canvas id="receitas"></canvas>
+                  </div>
+                  <div class="col-12 col-lg-6">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quae quasi minima autem est ipsa repellat veritatis repellendus debitis accusantium.</p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
 
-          <div class="container d-flex">
-          <div class="container p-5 row">
-            <!-- ------------------- Despesas e Receitas ------------------- -->
-            <div class="row" style="margin-bottom: 100px">
-              <div class="col-6">
-                <a href="despesas.php" style="text-decoration: none">
-                  <div class="card text-center">
-                    <div class="card-body card-totais">
-                      <h2 class="mt-5">Despesas do mês</h2>
-                      <!-- Telas maiores -->
-                      <i class="bi bi-graph-down-arrow bi-card"></i>
-                      <!-- <h5 class="card-title card-pc"><?php echo 'R$' . number_format($totalDespesas, 2, ",", "."); ?></h5> -->
-                      <!-- Telas maiores -->
-                      <!-- Telas menores -->
-                      <h5 class="card-title card-mobile">Total</h5>
-                      <!-- <p class="card-text card-mobile"><?php echo 'R$' . number_format($totalDespesas, 2, ",", "."); ?></p> -->
-                      <!-- Telas menores -->
-                    </div>
-                  </div>
-                </a>
-              </div>
-              <!-- <div class="col-6" style="width: 60px">
-                <a
-                  href="#"
-                  class="add-icon"
-                  data-bs-toggle="popover"
-                  data-bs-trigger="hover focus"
-                  data-bs-content="Adicionar despesa">
-                  <i class="bi bi-plus-circle-fill add-icon"></i>
-                </a>
-              </div> -->
-            </div>
-
-            <div class="row" style="margin-bottom: 100px">
-              <div class="col-6">
-                <a href="#">
-                  <div class="card text-center">
-                    <div class="card-body card-totais">
-                      <h2 class="mt-5">Receitas do mês</h2>
-                      <!-- Telas maiores -->
-                      <i class="bi bi-graph-up-arrow bi-card"></i>
-                      <!-- <h5 class="card-title card-pc"><?php echo 'R$' . number_format($totalReceitas, 2, ",", "."); ?></h5> -->
-                      <!-- Telas maiores -->
-                      <h5 class="card-title card-mobile">Total</h5>
-                      <!-- <p class="card-text card-mobile"><?php echo 'R$' . number_format($totalReceitas, 2, ",", "."); ?></p> -->
-                    </div>
-                  </div>
-                </a>
+          <div class="col-12 col-lg-6">
+            
+            <div class="container-estilizado">
+              <h5>Metas</h5>
+              <div class="container mt-5 mb-0">
+                <div class="row">
+                  <i class="bi bi-check2-circle col-2 text-black"></i>
+                  <p class="col-8">Comprar um carro</p>
+                  <hr>
+                  <i class="bi bi-check2-circle col-2 text-black"></i>
+                  <p class="col-8">1.000 reais investidos</p>
+                  <hr>
+                  <i class="bi bi-check2-circle col-2 text-black"></i>
+                  <p class="col-8">20.000 de reserva de emergência</p>
+                  <hr>
+                  <i class="bi bi-check2-circle col-2 text-black"></i>
+                  <p class="col-8">Reforma da casa</p>
+                  <hr>
+                </div>
               </div>
             </div>
 
-            <!-- ------------------- Despesas e Receitas fim ------------------- -->
-          </div>
-          <div class="container p-5 row" >
-                <!-- Card Suas metas -->
-                <div class="col-6 row">
-                  <a href="metas.php" style="text-decoration: none">
-                    <div class="card text-center">
-                      <div class="card-body card-totais">
-                        <h2 class="mt-5">Suas metas</h2>
-                        <!-- Telas maiores -->
-                        <i class="bi bi-star bi-card"></i>
-                        <!-- Telas menores -->
-                      </div>
-                    </div>
-                  </a>
-                </div>
-
-                <!-- Card Adicionar despesas e receitas -->
-                <div class="col-6 row" style="margin-top: 100px; margin-right: 100px">
-                  <a href="#">
-                    <div class="card text-center">
-                      <div class="card-body card-totais">
-                        <h2 class="mt-5">Adicionar despesas e receitas</h2>
-                        <!-- Telas maiores -->
-                        <i class="bi bi-plus-circle bi-card"></i>
-                      </div>
-                    </div>
-                  </a>
-                </div>
-              </div>
+            <div class="container-estilizado mt-3">
+              <a href="centroCusto.php">
+                <h5>Ir para contas <i class="bi bi-box-arrow-up-right text-black"></i></h5>
+              </a>
+            </div>
 
           </div>
         </div>
       </div>
+      
     </div>
   </div>
-
-
+  
+  
   <script src="../js/popup_notificacoes.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
   <script>
     // const patrimonioData = <?php echo json_encode($historicoPatrimonio); ?>
 
-    const ctx = document.getElementById("bar_chart");
+    const ctxDespesas = document.getElementById("despesas");
 
-    new Chart(ctx, {
-      type: "bar",
-      data: {
-        labels: [
-          "Jan",
-          "Fev",
-          "Mar",
-          "Abr",
-          "Mai",
-          "Jun",
-          "Jul",
-          "Ago",
-          "Set",
-          "Out",
-          "Nov",
-          "Dez",
-        ],
-        datasets: [{
-          label: "Evolução do patrimônio",
-          data: patrimonioData,
-          backgroundColor: "rgb(253, 136, 59)",
-        }, ],
-      },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true,
-          },
-        },
-      },
-    });
+    new Chart(ctxDespesas, {
+    type: 'doughnut',
+    data: {
+      labels: ['Compras do mês', 'Manutenção da moto', 'Viagem para Londres'],
+      datasets: [{
+        label: 'Valor(em R$)',
+        data: [450, 190, 800],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+
+
+
+  const ctxReceitas = document.getElementById('receitas');
+
+  new Chart(ctxReceitas, {
+    type: 'doughnut',
+    data: {
+      labels: ['Salário', 'Investimentos', 'Meu comércio'],
+      datasets: [{
+        label: 'Valor(em R$)',
+        data: [2400, 400, 7000],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
   </script>
 </body>
 
